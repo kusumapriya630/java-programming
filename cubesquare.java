5763728ce88df21a3e07a2b0e2bed55950769aa2
kusumapriya630 <113876556+kusumapriya630@users.noreply.github.com>
2022-09-29 13:14:33 +0530

    Add files via upload

---
+++ cubesquare.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        double num;
+        System.out.print("Enter the number: ");
+        num = sc.nextDouble();
+        System.out.println("Square of " + num + " is: " + Math.pow(num, 2));
+        System.out.println("Cube of " + num + " is: " + Math.pow(num, 3));
+    }
+}
+
